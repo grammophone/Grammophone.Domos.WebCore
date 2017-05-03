625642454a1906bc02a7aff2fde3268990ccf49d
grammophone <emmanuel.liossis@gmail.com>
2017-05-04 02:43:12 +0300

    Generalize RedirectionException to ActionException.

---
+++ Mvc/ActionException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Grammophone.Domos.Web.Mvc
+{
+	/// <summary>
+	/// An exception to force a redirection or other action,
+	/// usually because of insufficient conditions or other errors
+	/// during the execution of a controller.
+	/// </summary>
+	[Serializable]
+	public class ActionException : SystemException
+	{
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="targetActionResult">The action to be taken.</param>
+		/// <param name="innerException">Optional exception that caused the detour.</param>
+		public ActionException(ActionResult targetActionResult, Exception innerException = null)
+			: base("Exceptional controller action taken.", innerException)
+		{
+			if (targetActionResult == null) throw new ArgumentNullException(nameof(targetActionResult));
+
+			this.TargetActionResult = targetActionResult;
+		}
+
+		/// <summary>
+		/// Used for serialization.
+		/// </summary>
+		protected ActionException(
+			System.Runtime.Serialization.SerializationInfo info,
+			System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+		/// <summary>
+		/// The target action to be taken.
+		/// </summary>
+		public ActionResult TargetActionResult { get; }
+	}
+}
---
+++ Mvc/ActionExceptionFilterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Grammophone.Domos.Web.Mvc
+{
+	/// <summary>
+	/// Catches <see cref="ActionException"/> and executes
+	/// the <see cref="ActionException.TargetActionResult"/>.
+	/// </summary>
+	public class ActionExceptionFilterAttribute : FilterAttribute, IExceptionFilter
+	{
+		/// <summary>
+		/// Test whether the exception thrown is <see cref="ActionException"/>
+		/// an handle it.
+		/// </summary>
+		/// <param name="filterContext">The filter context.</param>
+		public void OnException(ExceptionContext filterContext)
+		{
+			var actionException = filterContext.Exception as ActionException;
+
+			if (actionException != null)
+			{
+				filterContext.Result = actionException.TargetActionResult;
+
+				filterContext.ExceptionHandled = true;
+			}
+		}
+	}
+}
