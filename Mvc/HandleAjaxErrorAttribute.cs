75fa49bfe35ed5ae40061058683ebfaf46ff6144
grammophone <emmanuel.liossis@gmail.com>
2017-07-25 15:55:06 +0300

    Enhance AJAX error handling to include referential constraint and access denial errors.

--- Mvc/HandleAjaxErrorAttribute.cs
+++ Mvc/HandleAjaxErrorAttribute.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Grammophone.DataAccess;
 using Grammophone.Domos.Logic;
 using Grammophone.Domos.Web.Models;
 
@@ -20,24 +22,77 @@ namespace Grammophone.Domos.Web.Mvc
 	public class HandleAjaxErrorAttribute : FilterAttribute, IExceptionFilter
 	{
 		/// <summary>
-		/// If the request is an AJAX one and the exception is of type <see cref="UserException"/>,
-		/// transform it into a <see cref="UserErrorModel"/> and return it as a JSON.
+		/// If the request is an AJAX one and the exception is of type <see cref="UserException"/>
+		/// or a descendant of <see cref="AccessDeniedException"/> or of <see cref="IntegrityViolationException"/>,
+		/// transform it into a <see cref="UserErrorModel"/> and return it as a JSON
+		/// with an appropriate error HTTP status code.
 		/// </summary>
-		/// <param name="filterContext"></param>
+		/// <param name="filterContext">The context containing the exception.</param>
+		/// <remarks>
+		/// <see cref="AccessDeniedException"/> and descendants yeild HTTP status code 403 "Forbidden".
+		/// <see cref="UniqueConstraintViolationException"/> and <see cref="ReferentialConstraintViolationException"/>
+		/// yield HTTP Status 409 "Conflict".
+		/// All other rexceptions result to 500 "Internal Server Error".
+		/// </remarks>
 		public void OnException(ExceptionContext filterContext)
 		{
 			if (filterContext.HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
 			{
-				if (filterContext.Exception is UserException userException)
+				var exception = filterContext.Exception;
+
+				UserErrorModel userErrorModel = null;
+
+				var statusCode = HttpStatusCode.InternalServerError;
+
+				if (exception is AccessDeniedException
+					|| exception is IntegrityViolationException)
+				{
+					string userMessage;
+
+					switch (exception)
+					{
+						case AccessDeniedException accessDeniedException:
+							statusCode = HttpStatusCode.Forbidden;
+							userMessage = ErrorMessages.ACCESS_DENIED;
+
+							var telemetry = new Microsoft.ApplicationInsights.TelemetryClient();
+
+							telemetry.TrackException(exception);
+							break;
+
+						case UniqueConstraintViolationException uniqueConstraintViolationException:
+							statusCode = HttpStatusCode.Conflict;
+							userMessage = ErrorMessages.UNIQUENESS_CONSTRAINT_VIOLATION;
+							break;
+
+						case ReferentialConstraintViolationException referentialConstraintViolationException:
+							statusCode = HttpStatusCode.Conflict;
+							userMessage = ErrorMessages.RELATIONAL_CONSTRAINT_VIOLATION;
+							break;
+
+						default:
+							statusCode = HttpStatusCode.InternalServerError;
+							userMessage = ErrorMessages.GENERIC_ERROR;
+							break;
+					}
+
+					userErrorModel = new UserErrorModel(userMessage);
+				}
+				else if (exception is UserException userException)
+				{
+					userErrorModel = new UserErrorModel(userException);
+				}
+
+				if (userErrorModel != null)
 				{
 					filterContext.Result = new JsonResult
 					{
-						Data = new UserErrorModel(userException),
+						Data = userErrorModel,
 						ContentEncoding = Encoding.UTF8,
 						JsonRequestBehavior = JsonRequestBehavior.AllowGet
 					};
 
-					filterContext.HttpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.InternalServerError;
+					filterContext.HttpContext.Response.StatusCode = (int)statusCode;
 
 					filterContext.ExceptionHandled = true;
 				}
