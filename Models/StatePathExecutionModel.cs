bc5163795039a028e8e311160333c252986069e2
grammophone <emmanuel.liossis@gmail.com>
2020-02-27 15:12:56 -0500

    Add models for state path execution on stateful objects.

---
+++ Models/StatePathExecutionModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Grammophone.Domos.Domain;
+using Grammophone.Domos.Domain.Workflow;
+using Grammophone.Domos.Logic;
+
+namespace Grammophone.Domos.Web.Models
+{
+	/// <summary>
+	/// Model for the execution of a state path
+	/// on a stateful object.
+	/// </summary>
+	[Serializable]
+	public class StatePathExecutionModel : ActionExecutionModel
+	{
+		#region Private fields
+
+		private readonly Func<string, IReadOnlyDictionary<string, ParameterSpecification>> parameterSpecificationFunction;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="parameterSpecificationFunction">A function to supply the parameters neeed for execution of a state path by code name.</param>
+		/// <param name="statePathCodeName">The code name of the state path being executed.</param>
+		public StatePathExecutionModel(Func<string, IReadOnlyDictionary<string, ParameterSpecification>> parameterSpecificationFunction, string statePathCodeName)
+			: base(statePathCodeName)
+		{
+			if (parameterSpecificationFunction == null) throw new ArgumentNullException(nameof(parameterSpecificationFunction));
+
+			this.parameterSpecificationFunction = parameterSpecificationFunction;
+		}
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="parameterSpecificationFunction">A function to supply the parameters neeed for execution of a state path by code name.</param>
+		/// <param name="valueProvider">The value provider which will supply the ActionCodeName property.</param>
+		/// <param name="prefix">Optional prefix for the key in <paramref name="valueProvider"/>.</param>
+		public StatePathExecutionModel(
+			Func<string, IReadOnlyDictionary<string, ParameterSpecification>> parameterSpecificationFunction,
+			IValueProvider valueProvider,
+			string prefix = null)
+			: base(valueProvider, prefix)
+		{
+			if (parameterSpecificationFunction == null) throw new ArgumentNullException(nameof(parameterSpecificationFunction));
+
+			this.parameterSpecificationFunction = parameterSpecificationFunction;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The ID of the stateful object upon which the state path will be executed.
+		/// </summary>
+		public long StatefulID { get; set; }
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Uses the function supplied in the constructor to get the parameter specifications for the execution of a state path.
+		/// </summary>
+		/// <param name="statePathCodeName">The code name of the state path to be executed.</param>
+		/// <returns>Returns a dictionary of parameter specifications by parameter name.</returns>
+		protected internal override IReadOnlyDictionary<string, ParameterSpecification> GetParameterSpecifications(string statePathCodeName)
+			=> parameterSpecificationFunction(statePathCodeName);
+
+		#endregion
+	}
+
+	/// <summary>
+	/// Model for the execution of a state path
+	/// on a stateful object associated with a workflow manager.
+	/// </summary>
+	/// <typeparam name="U">The type of the user, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="ST">The type of state transitions, derived from <see cref="StateTransition{U}"/>.</typeparam>
+	/// <typeparam name="SO">The type of stateful object, implementing <see cref="IStateful{U, ST}"/>.</typeparam>
+	public class StatePathExecutionModel<U, ST, SO> : StatePathExecutionModel
+		where U : User
+		where ST : StateTransition<U>
+		where SO : IStateful<U, ST>
+	{
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="workflowManager">The associated workflow manager.</param>
+		/// <param name="statePathCodeName">The code name of the state path being executed.</param>
+		public StatePathExecutionModel(IWorkflowManager<U, ST, SO> workflowManager, string statePathCodeName)
+			: base(workflowManager.GetPathParameterSpecifications, statePathCodeName)
+		{
+		}
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="workflowManager">The associated workflow manager.</param>
+		/// <param name="valueProvider">The value provider which will supply the ActionCodeName property.</param>
+		/// <param name="prefix">Optional prefix for the key in <paramref name="valueProvider"/>.</param>
+		public StatePathExecutionModel(
+			IWorkflowManager<U, ST, SO> workflowManager,
+			IValueProvider valueProvider,
+			string prefix = null)
+			: base(workflowManager.GetPathParameterSpecifications, valueProvider, prefix)
+		{
+		}
+	}
+}
---
+++ Models/StatefulExecutionModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grammophone.Domos.Domain;
+using Grammophone.Domos.Domain.Workflow;
+using Grammophone.Domos.Logic;
+
+namespace Grammophone.Domos.Web.Models
+{
+	/// <summary>
+	/// Model for executing a state path against a stateful object.
+	/// </summary>
+	/// <typeparam name="SO">The type of the stateful object.</typeparam>
+	public class StatefulExecutionModel<SO>
+		where SO : IStateful
+	{
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="stateful">The stateful object to execute the path on.</param>
+		/// <param name="statePath">The state path to execute.</param>
+		/// <param name="execution">The path execution model.</param>
+		public StatefulExecutionModel(SO stateful, StatePath statePath, StatePathExecutionModel execution)
+		{
+			if (stateful == null) throw new ArgumentNullException(nameof(stateful));
+			if (statePath == null) throw new ArgumentNullException(nameof(statePath));
+			if (execution == null) throw new ArgumentNullException(nameof(execution));
+
+			this.Stateful = stateful;
+			this.StatePath = statePath;
+			this.Execution = execution;
+		}
+
+		/// <summary>
+		/// The stateful object.
+		/// </summary>
+		public SO Stateful { get; }
+
+		/// <summary>
+		/// The state path to be executed on the <see cref="Stateful"/>.
+		/// </summary>
+		public StatePath StatePath { get; }
+
+		/// <summary>
+		/// Contains the parameters of the execution of the <see cref="StatePath"/>
+		/// against the <see cref="Stateful"/>.
+		/// </summary>
+		public StatePathExecutionModel Execution { get; }
+	}
+
+	/// <summary>
+	/// Model for executing a state path against a stateful object.
+	/// </summary>
+	/// <typeparam name="U">The type of the user, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="ST">The type of state transitions, derived from <see cref="StateTransition{U}"/>.</typeparam>
+	/// <typeparam name="SO">The type of stateful object, implementing <see cref="IStateful{U, ST}"/>.</typeparam>
+	public class StatefulExecutionModel<U, ST, SO> : StatefulExecutionModel<SO>
+		where U : User
+		where ST : StateTransition<U>
+		where SO : IStateful<U, ST>
+	{
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="stateful">The stateful object to execute the path on.</param>
+		/// <param name="statePath">The state path to execute.</param>
+		/// <param name="workflowManager">The workflow manager for the <paramref name="stateful"/> object.</param>
+		public StatefulExecutionModel(SO stateful, StatePath statePath, IWorkflowManager<U, ST, SO> workflowManager)
+			: base(stateful, statePath, CreateStatePathExecution(stateful, statePath, workflowManager))
+		{
+		}
+
+		private static StatePathExecutionModel CreateStatePathExecution(SO stateful, StatePath statePath, IWorkflowManager<U, ST, SO> workflowManager)
+		{
+			if (stateful == null) throw new ArgumentNullException(nameof(stateful));
+			if (statePath == null) throw new ArgumentNullException(nameof(statePath));
+			if (workflowManager == null) throw new ArgumentNullException(nameof(workflowManager));
+
+			return new StatePathExecutionModel<U, ST, SO>(workflowManager, statePath.CodeName)
+			{
+				StatefulID = stateful.ID
+			};
+		}
+	}
+}
