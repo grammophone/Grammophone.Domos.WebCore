66a7fb8f81738b8697d5dbdb1ec3a6d066aa8c7d
grammophone <emmanuel.liossis@gmail.com>
2021-06-22 22:48:41 -0400

    First version of special binding of collections whose elements implement IEntityWithKey interface.

--- Mvc/ModelBinding/EntityCollectionModelBinderProvider.cs
+++ Mvc/ModelBinding/EntityCollectionModelBinderProvider.cs
@@ -12,14 +12,14 @@ using Microsoft.Extensions.Options;
 namespace Grammophone.Domos.WebCore.Mvc.ModelBinding
 {
 	/// <summary>
-	/// Provider for <see cref="EntityCollectionModelBinder{E}"/>,
+	/// Provider for <see cref="EntityCollectionModelBinder{K, E}"/>,
 	/// for collection models implementing <see cref="ICollection{T}"/> having elements implementing <see cref="IEntityWithID{K}"/>.
 	/// </summary>
 	public class EntityCollectionModelBinderProvider : IModelBinderProvider
 	{
 		/// <summary>
 		/// If the model is a collection implementing <see cref="ICollection{T}"/> having elements implementing <see cref="IEntityWithID{K}"/>,
-		/// returns an <see cref="EntityCollectionModelBinder{E}"/>, else null.
+		/// returns an <see cref="EntityCollectionModelBinder{K, E}"/>, else null.
 		/// </summary>
 		/// <param name="context">The binder context.</param>
 		public IModelBinder GetBinder(ModelBinderProviderContext context)
@@ -29,7 +29,7 @@ namespace Grammophone.Domos.WebCore.Mvc.ModelBinding
 			Type modelType = context.Metadata.ModelType;
 
 			// Search for ICollection<E> in the implemented interfaces.
-			foreach (var collectionInterfaceType in modelType.GetInterfaces())
+			foreach (var collectionInterfaceType in GetInterfacesOfType(modelType))
 			{
 				if (!collectionInterfaceType.IsGenericType) continue;
 
@@ -45,8 +45,10 @@ namespace Grammophone.Domos.WebCore.Mvc.ModelBinding
 
 					if (elementInterfaceType.GetGenericTypeDefinition() != typeof(IEntityWithID<>)) continue;
 
+					Type keyType = elementInterfaceType.GenericTypeArguments[0];
+
 					// If found, return an EntityCollectionModelBinder<E>.
-					return CreateBinder(context, elementType);
+					return CreateBinder(context, keyType, elementType);
 				}
 			}
 
@@ -54,21 +56,39 @@ namespace Grammophone.Domos.WebCore.Mvc.ModelBinding
 			return null;
 		}
 
-		private IModelBinder CreateBinder(ModelBinderProviderContext context, Type elementType)
+		private IModelBinder CreateBinder(ModelBinderProviderContext context, Type keyType, Type elementType)
 		{
-			var binderType = typeof(EntityCollectionModelBinder<>).MakeGenericType(elementType);
+			var binderType = typeof(EntityCollectionModelBinder<,>).MakeGenericType(keyType, elementType);
 			var elementBinder = context.CreateBinder(context.MetadataProvider.GetMetadataForType(elementType));
 
 			var loggerFactory = context.Services.GetRequiredService<ILoggerFactory>();
 			var mvcOptions = context.Services.GetRequiredService<IOptions<MvcOptions>>().Value;
 
+			var modelMetadataProvider = context.Services.GetRequiredService<IModelMetadataProvider>();
+
+			var keyMetadata = modelMetadataProvider.GetMetadataForType(keyType);
+
+			var keyBinder = context.CreateBinder(keyMetadata);
+
 			return (IModelBinder)Activator.CreateInstance(
 				binderType,
+				keyBinder,
+				keyMetadata,
 				elementBinder,
 				loggerFactory,
 				true /* allowValidatingTopLevelNodes */,
 				mvcOptions
 			);
+		}
+
+		private IEnumerable<Type> GetInterfacesOfType(Type type)
+		{
+			if (type.IsInterface) yield return type;
+
+			foreach (var interfaceType in type.GetInterfaces())
+			{
+				yield return interfaceType;
+			}
 		}
 	}
 }
